77b69c6793806f61af53d4006f8f86178d104b58
Natalia Grzywalska <n.grzywalska@gmail.com>
2024-04-25 13:47:34 +0200

    fix for widget_test to work

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
-import 'package:expense_tracker_app/bloc/expense_bloc.dart';
 import 'package:expense_tracker_app/bloc/add_expense_cubit.dart';
+import 'package:expense_tracker_app/bloc/expense_bloc.dart';
 import 'package:expense_tracker_app/expenses_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +18,10 @@ class MainApp extends StatelessWidget {
         BlocProvider(create: (context) => ExpenseBloc()),
         BlocProvider(create: (context) => AddExpenseCubit()),
       ],
-      child: const MaterialApp(
-        home: ExpensesScreen(),
+      child: MaterialApp(
+        theme: ThemeData().copyWith(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+        home: const ExpensesScreen(),
       ),
     );
   }
